6416b70a9aca703bee9d55a2246638430cfb28d6
Tangusjr <gilbertofke@gmail.com>
2025-04-14 11:33:47 +0300

    Chore: Grabbed DOM elements for manipulation

---
+++ app.js
@@ -0,0 +1,8 @@
+//This section demonstartes how to grab DOM Elements for manipulation
+const form = document.getElementById('resourceForm');
+const resourceList=document.getElementById("resourceList");
+const counter=document.getElementById('counter');
+const searchInput=document.querySelector('.search-input');
+const filterButtons=document.querySelectorAll('.filter-btn');
+
+//This section 
